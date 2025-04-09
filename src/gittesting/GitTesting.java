71b12292694750e261e76572c3b25f295ab470f7
ErinC <ErinC@Lab-06.kegrp.co.za>
2025-04-09 10:21:44 +0200

    Testing food Commit

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -6,6 +6,7 @@ public class GitTesting {
     
     public static void main(String[] args) {
         String testingClassmate = "Lennie";
+        String testingFood = "Pizza";
         System.out.println("Lennie");
         System.out.println("Hello Git");
         System.out.println("Testing");

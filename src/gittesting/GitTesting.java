dbceb029392b6b833273686501e671da5992ecce
ErinC <ErinC@Lab-06.kegrp.co.za>
2025-04-09 10:21:20 +0200

    Testing classmate Commit

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -5,8 +5,12 @@ public class GitTesting {
 
     
     public static void main(String[] args) {
+        String testingClassmate = "Lennie";
+        System.out.println("Lennie");
         System.out.println("Hello Git");
         System.out.println("Testing");
+        
+        
 
     }
     

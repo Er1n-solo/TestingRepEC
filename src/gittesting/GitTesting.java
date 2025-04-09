0ca4e874c1fb23400159d8000fdff9067589de76
ErinC <ErinC@Lab-06.kegrp.co.za>
2025-04-09 10:23:14 +0200

    Testing my name Commit

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -7,10 +7,11 @@ public class GitTesting {
     public static void main(String[] args) {
         String testingClassmate = "Lennie";
         String testingFood = "Pizza";
-        System.out.println("Lennie");
+        System.out.println(testingClassmate);
+        System.out.println(testingFood);
         System.out.println("Hello Git");
         System.out.println("Testing");
-        
+        System.out.println("My name is Erin");
         
 
     }

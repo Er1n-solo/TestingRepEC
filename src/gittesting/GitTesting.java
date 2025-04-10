75b7ea7fe16958ec7af07ed91de72d3873e737f6
Er1n-solo <erinclegg111@gmail.com>
2025-04-10 11:47:12 +0200

    Reapply "Testing delete and add hello Commit"
    This reverts commit beb70b1d466e867c6615703398f819d0a6a1b48a.

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -5,18 +5,7 @@ public class GitTesting {
 
     
     public static void main(String[] args) {
-        String testingClassmate = "Lennie";
-        String testingFood = "Pizza";
-        System.out.println(testingClassmate);
-        System.out.println(testingFood);
-        System.out.println("Hello Git");
-        System.out.println("Testing");
-        System.out.println("My name is Erin");
-        System.out.println("Her name is Luka");
-        System.out.println("Watching a movie");
-        
-        int Testing= 1;
-        
+       System.out.println("Hello.");
 
     }
     
